d5d83e5f770f5100b16717822d0763d65b0e59f9
Ronaldo <awademeronaldoo@gmail.com>
2025-01-28 23:19:37 +0100

    Mise en place du protocolDePointDeControl pour générer et formater les points de ravitaillement.

--- exo_02.dart
+++ exo_02.dart
@@ -6,8 +6,8 @@ void main() {
 
 void calculerTrajet(int distance) {
   //protocolEnergique(distance);
-  protocolDeGestionEnergie(distance);
-  //protocolDePointDeControl(distance);
+  //protocolDeGestionEnergie(distance);
+  protocolDePointDeControl(distance);
   //protocolDeSecurite(distance);
 }
 
@@ -41,4 +41,21 @@ void protocolDeGestionEnergie(int distance) {
     consommation += (consommation * 0.2);
   }
   print("Consommation estimée : $consommation unités énergétiques");
+}
+
+void protocolDePointDeControl(int distance) {
+  if (distance < 0) {
+    print("Distance invalide !");
+    return;
+  }
+  List<int> pointsRavitaillement = [];
+  for (int km = 5; km <= distance; km += 5) {
+    pointsRavitaillement.add(km);
+  }
+
+  List<String> pointsFormates = pointsRavitaillement.map((point) {
+    return point.toString().padLeft(3, '0') + " km";
+  }).toList();
+  print("Points de ravitaillement : $pointsRavitaillement");
+  print("Formaté pour LED : $pointsFormates");
 }
