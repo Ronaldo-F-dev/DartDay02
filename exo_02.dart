e8e224c805f6df995bee7f0b75e059cc90d4a74d
Ronaldo <awademeronaldoo@gmail.com>
2025-01-28 23:16:27 +0100

    mplémentation du protocolEnergique pour gérer les modes de trajet (Route rapide ou Route éco) et calculer les cycles de charge.

--- exo_02.dart
+++ exo_02.dart
@@ -5,8 +5,28 @@ void main() {
 }
 
 void calculerTrajet(int distance) {
-  //protocolEnergique(distance);
+  protocolEnergique(distance);
   //protocolDeGestionEnergie(distance);
   //protocolDePointDeControl(distance);
   //protocolDeSecurite(distance);
+}
+
+void protocolEnergique(int distance) {
+  if (distance < 0) {
+    print("Distance invalide !");
+    return;
+  }
+
+  if (distance % 2 == 0) {
+    print("Activation mode Route rapide");
+  } else {
+    print("Activation mode Route éco");
+  }
+  int quotient;
+  if (distance % 10 == 0) {
+    quotient = (distance / 10).floor() - 1;
+  } else {
+    quotient = (distance / 10).floor();
+  }
+  print("$quotient recharge(s) + charge initiale");
 }
