87be92ac721bc30ef547206ef214bebd5df797a3
Ronaldo <awademeronaldoo@gmail.com>
2025-01-28 23:13:21 +0100

    Ajout de la fonction principale calculerTrajet pour orchestrer les protocoles énergétiques et de sécurité.

---
+++ exo_02.dart
@@ -0,0 +1,12 @@
+import 'dart:math';
+
+void main() {
+  calculerTrajet(56);
+}
+
+void calculerTrajet(int distance) {
+  //protocolEnergique(distance);
+  //protocolDeGestionEnergie(distance);
+  //protocolDePointDeControl(distance);
+  //protocolDeSecurite(distance);
+}
