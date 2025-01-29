a5f9c7388f6964e1619b7104861830849bd334d0
Ronaldo <awademeronaldoo@gmail.com>
2025-01-29 10:28:08 +0100

    création de la méthode ajouterLieu() pour ajouter un Lieu

---
+++ exo_05.dart
@@ -0,0 +1,24 @@
+List<Map<String, dynamic>> carteTresor = [];
+
+void ajouterLieu(String nom, String description, double distance) {
+  carteTresor.add({
+    "nom": nom,
+    "description": description,
+    "distance": distance,
+    "explorateurs": []
+  });
+  print("Lieu ajouté : $nom");
+}
+
+void main() {
+  ajouterLieu("Île Perdue", "Un lieu mystérieux avec un danger caché.", 15.3);
+  ajouterLieu("Temple Maudit",
+      "Un ancien temple où des explorateurs ont disparu.", 25.7);
+  ajouterLieu("Plage Ensoleillée", "Un paradis tropical paisible.", 5.0);
+  //enregistrerExplorateur("Île Perdue", "GABIAM Honorine");
+  //enregistrerExplorateur("Temple Maudit", "AWADEME Ronaldo");
+  //analyserIndices();
+  //afficherCarte();
+  //retirerLieu("Temple Maudit");
+  //afficherCarte();
+}
